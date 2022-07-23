1deef37bd440c6393303f7e3d54edd39843787d6
chubo <2569829371@qq.com>
2022-07-23 17:28:09 +0800

    second add

--- 44_src_element-ui组件库/App.vue
+++ 44_src_element-ui组件库/App.vue
@@ -8,7 +8,7 @@
 			<el-button type="success">成功按钮</el-button>
 			<el-button type="info">信息按钮</el-button>
 			<el-button type="warning">警告按钮</el-button>
-			<el-button type="danger">危险按钮</el-button>
+			
 		</atguigu-row>
 		<atguigu-date-picker
       type="date"
